6c7eb3e021b39c80936757f423a0cb137bb335e7
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2020-12-06 00:57:24 +0900

    :cactus: add seed
    seedファイルを作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,72 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $users = $this->getSeedUsers();
+        $size = count($users);
+        for ($i = 0; $i < $size; $i++) {
+            DB::table('users')->insert($users[$i]);
+        }
+
+        $court = $this->getSeedCourt();
+        DB::table('courts')->insert($court);
+
+        $reserves = $this->getSeedReserves();
+        $size = count($reserves);
+        for ($i = 1; $i <= $size; $i++) {
+            DB::table('reserves')->insert($reserves[$i]);
+        }
+
+    }
+
+    public function getSeedUsers()
+    {
+        $users[0] = [
+            'name' => 'test1',
+            'email' => 'aaa@aaa',
+            'email_verified_at' => null,
+            'password' => '1234'
+        ];
+
+        $users[1] = [
+            'name' => 'test2',
+            'email' => 'bbb@bbb',
+            'email_verified_at' => null,
+            'password' => '1234'
+        ];
+
+        $users[2] = [
+            'name' => 'test3',
+            'email' => 'ccc@ccc',
+            'email_verified_at' => null,
+            'password' => '1234'
+        ];
+        
+
+        return $users;
+    }
+
+    public function getSeedCourt()
+    {
+        return [
+            'type' => 1
+        ];
+    }
+
+    public function getSeedReserves()
+    {
+        $userIds = [1, 2, 3];
+        $courtId = 1;
+        $tmpDateTime = "2020-12-07 09:00:00";
+        $reserves = [];
+        for ($i = 1; $i <= 20; $i++) {
+            $reserves[$i]['user_id'] = $userIds[$i % 3];
+            $reserves[$i]['court_id'] = $courtId;
+            $tmpDateTime = date('Y-m-d H:i:s', strtotime($tmpDateTime));
+            $reserves[$i]['start_time'] = $tmpDateTime;
+            $tmpDateTime = date('Y-m-d H:i:s', strtotime($tmpDateTime . "+30 minute"));
+            $reserves[$i]['end_time'] = $tmpDateTime;
+        }
+
+        return $reserves;
     }
 }
